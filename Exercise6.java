00fc8113e60d56c96af7cd5cd6651c7259d406e7
ssahaalbany <ssaha@albanybeck.com>
2022-07-26 00:53:26 +0100

    1st Draft Done

--- Exercise6.java
+++ Exercise6.java
@@ -40,9 +40,9 @@ return this.y;
 }
 //Declaring a method to return the co-ordinates using toString
 
-public String getCoordinates ()
+public String toString()
 {
-    return ("The co-ordinates pair is: " + "("+ getXcoordinate()+"," + getYcoordinate()+ ")");
+    return ("The value of x and y is: " + "("+ getXcoordinate()+"," + getYcoordinate()+ ")");
 }
 
 
@@ -57,12 +57,12 @@ abstract class Shape
     private double x;
     private double y;
 //Declaring Constructor
-public Shape(String name, String colour)
+public Shape(String name, String colour, double x, double y)
 {
     this.name = name;
     this.colour = colour;
-   // this. x = x;
-   // this. y = y;
+   this. x = x;
+   this. y = y;
 }
 
 //Setter Methods
@@ -109,20 +109,24 @@ public double getYcoordinate()
 return this.y;
 }
     
-public String getDetailstoString()
+public String toString()
 {
     return ("Name: " + this.name+ "\nColour:" + this.colour+ "\n" + "The co-ordinates pair is: " + "("+ getXcoordinate()+"," + getYcoordinate()+ ")" );
 }
 }
+
+
+
+
  class Rectangle extends Shape
 {
     private double height;
     private double width;
-    private Point p;
+    //private Point p;
     
-    public Rectangle(double height, double width, String name, String colour)
+    public Rectangle(double height, double width, String name, String colour,double x, double y)
     {
-        super(name,colour);
+        super(name,colour,x,y);
         this.height = height;
         this.width = width;
 
@@ -150,32 +154,34 @@ public String getDetailstoString()
         return this.width;
     }
     
-    
-    public String getRectDetailstoString()
+    /*
+    public String toString()
     {
-        return "Name:" + super.getName()+ "\n Colour:" + super.getColour()+ "\nThe height of rectangle is:" + getHeight() + "\nThe width of Rectangle is: " + getWidth();
+        return super.toString()+ "\nThe height of rectangle is:" + getHeight() + "\nThe width of Rectangle is: " + getWidth() + isSquare() +  getCentrePoint()  ;
     }
+    */
 
     public boolean isSquare()
     {
         if ((this.height!=this.width) && (this.height>0) && (this.width>0))
         {
            
-            System.out.println("True\nThis is a Rectangle");
+            System.out.println("This is a Rectangle");
              return true;
         }
-        else if  ((this.height!=this.width) && (this.height<0) && (this.width<0))
+        else if  ( (this.height<0) || (this.width<0) )
         {
             
-            System.out.println("False \n Less than 0 value is not accepted");
+           System.out.println("Less than 0 value is not accepted");
             return false;
+
         }
 
 
         else 
         {
             
-            System.out.println("False\nThis is a square");
+           System.out.println("\nThis is a square");
             return false;
         }
     }
@@ -188,22 +194,57 @@ public String getDetailstoString()
     
     public Point getCentrePoint()
     { 
-        double x,y;
-         x  = getHeight()/2;
-         y = getWidth()/2;
-        Point p = new Point(x,y);
-        return p;
+        
+        double x1,y1;
+       x1 = getHeight()/2;
+        y1 =getWidth()/2;
+
+         //System.out.println(x1);
+         //System.out.println(y1);
+         
+        //Point p = new Point(x,y);
+        return new Point(x1,y1);
     }
     
-    public String  getAlltoString()
+    public String toString()
     {
-        return super.getDetailstoString() + "\nArea:" + getArea()+ "\ngetCentrePoint: " + getCentrePoint();
+        return isSquare() + "\n" + super.toString()+"\n" + "The height of rectangle is:" + getHeight() + "\nThe width of Rectangle is: " + getWidth() + "\nArea of Rectangle:" + getArea()+ "\n" +  "\n" + getCentrePoint()  ;
     }
-    
+
     
         
 }
 
+class Circle extends Shape
+{
+    private double r;
+    public Circle(String name, String colour, double x, double y,double r)
+    {
+        super(name,colour,x,y);
+        this.r  = r;
+    }
+
+
+    public double getRadius()
+    {
+        return this.r;
+    }
+    public Point getCentrePoint()
+    {
+        return new Point(super.getXcoordinate(),super.getYcoordinate());
+    }
+    public double getArea()
+    {
+        double area;
+        area = Math.PI * Math.pow(r,2);
+        return area;
+    }
+
+    public String toString()
+    {
+        return super.toString() + "\nArea of Circle:" + getArea() ;
+    }
+}
 
 
 
@@ -214,15 +255,31 @@ class Exercise6{
     {   
         //Point p = new Point(2,4);
        //System.out.println (p.getCoordinates());
-       Rectangle  r1 = new Rectangle (15,20, "R1", "Red");
-       Rectangle  r2 = new Rectangle  (10,20, "R2", "Blue");
-       Rectangle  r3 = new Rectangle (4,8,"R3","Green");
+       Rectangle  r1 = new Rectangle (15,20, "R1", "Red",4,5);
+       Rectangle  r2 = new Rectangle  (10,10, "R2", "Blue",3,4);
+       Rectangle  r3 = new Rectangle (4,-8,"R3","Green",5,6);
         
         Rectangle[] r = {r1,r2,r3};
         for (Rectangle rctngl:r)
         {
-            System.out.println(rctngl.getAlltoString());
+            System.out.println(rctngl+ "\n\n");
         }
+
+
+        System.out.println("==============================================");
+
+        Circle c1 = new Circle("C1", "Grey",2,-2,4);
+        Circle c2 = new Circle("C2", "Black",1,1,2.5);
+        Circle c3 = new Circle("C3", "White",0,10,9.96);
+
+        Circle[] c = {c1,c2,c3};
+
+        for (Circle crcl :c )
+
+        {
+            System.out.println(crcl + "\n\n");
+        }
+
         
     }
 }
