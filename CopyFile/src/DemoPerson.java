02c703bbc03821833e9f361a0d1b39ffedaa8b6a
ssahaalbany <ssaha@albanybeck.com>
2022-07-27 02:15:02 +0100

    Test Source Directory

---
+++ CopyFile/src/DemoPerson.java
@@ -0,0 +1,9 @@
+class DemoPerson
+{
+	public static void main (String[]args)
+	{
+		Person p = new Person();
+		p.setAge(10);
+		System.out.println(p.getAge());
+	}
+}
---
+++ CopyFile/src/Person.java
@@ -0,0 +1,20 @@
+public class Person
+{
+	private int age;
+	public Person()
+	{
+		this.age = age;
+
+	}
+
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+}
